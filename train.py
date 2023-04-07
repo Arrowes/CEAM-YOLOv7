3fce9752d79357316ba849b48ff23595e2403631
Arrowes <395841716@qq.com>
2023-04-07 20:41:23 +0800

    Organized
    20230407

--- train.py
+++ train.py
@@ -547,11 +547,11 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default=ROOT / 'yolov7-tiny.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='models/v7_cfg/deploy/yolov7-gam3.yaml', help='CEAM-YOLOv7 model')
+    parser.add_argument('--weights', type=str, default=ROOT / 'weight/yolov7-tiny.pt', help='initial weights path')
+    parser.add_argument('--cfg', type=str, default='models/CEAM-YOLOv7.yaml', help='CEAM-YOLOv7 model')
     parser.add_argument('--data', type=str, default=ROOT / 'mydata/Driver.3/data.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default=ROOT / 'data/hyps/hyp.scratch-v7.yaml', help='hyperparameters path')
-    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--batch-size', type=int, default=8, help='total batch size for all GPUs,  -1 for autobatch')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
     parser.add_argument('--rect', action='store_true', help='rectangular training')
@@ -571,7 +571,7 @@ def parse_opt(known=False):
     parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
     parser.add_argument('--workers', type=int, default=8, help='max dataloader workers (per RANK in DDP mode)')
     parser.add_argument('--project', default=ROOT / 'runs/train', help='save to project/name')
-    parser.add_argument('--name', default='exp yolov4', help='save to project/name')
+    parser.add_argument('--name', default='exp', help='save to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--quad', action='store_true', help='quad dataloader')
     parser.add_argument('--cos-lr', action='store_true', help='cosine LR scheduler')
